a553f705c77adaa4d4bb72a79213312b455cff85
Keichi Takahashi <keichi.t@me.com>
2014-08-05 01:40:49 +0900

    Fixes #1
    Rewritten with nan package. Now it should be compatible with node 0.8 -
    0.12.

--- osx_tag.mm
+++ osx_tag.mm
@@ -1,362 +1,307 @@
 #import <Foundation/Foundation.h>
 
 #include <node.h>
-#include <v8.h>
+#include <nan.h>
 #include <iostream>
 #include <cstring>
 
 using namespace v8;
 
-struct async_data
-{
-    const char* path;
-    const char* error;
-    NSMutableSet* tags;
-    NSMutableSet* updatingTags;
-    Persistent<Function> callback;
+class GetTagsWorker : public NanAsyncWorker {
+public:
+    GetTagsWorker(NanCallback *callback, const char *path) : NanAsyncWorker(callback), path(path) {}
+    ~GetTagsWorker() {}
+
+    void Execute()
+    {
+        NSString *s = [NSString stringWithUTF8String:this->path];
+        NSURL *url = [NSURL fileURLWithPath:s];
+        NSError *error = nil;
+        NSArray *tags;
+        
+        [url getResourceValue:&tags forKey:NSURLTagNamesKey error:&error];
+
+        if (error) {
+            SetErrorMessage([[error localizedDescription] UTF8String]);
+        } else {
+            this->tags = [NSMutableSet setWithArray:tags];    
+        }
+    }
+
+    void HandleOKCallback()
+    {
+        NanScope();
+
+        Handle<Value> args[2] = { NanNull(), NanNew<v8::Array>([this->tags count]) };
+
+        int i = 0;
+        for (NSString *item in this->tags) {
+            args[1].As<Array>()->Set(i++, NanNew([item UTF8String]));
+        }
+
+        callback->Call(2, args);
+    }
+private:
+    NSMutableSet *tags;
+    const char *path;
 };
 
-void workGetTags(uv_work_t* req)
+class SetTagsWorker : public NanAsyncWorker {
+public:
+    SetTagsWorker(NanCallback *callback, const char *path, NSMutableSet *tags) : NanAsyncWorker(callback), path(path), tags(tags) {}
+    ~SetTagsWorker() {}
+
+    void Execute()
+    {
+        NSString *s = [NSString stringWithUTF8String:this->path];
+        NSURL *url = [NSURL fileURLWithPath:s];
+        NSError *error = nil;
+        
+        [url setResourceValue:[this->tags allObjects] forKey:NSURLTagNamesKey error:&error];
+
+        if (error) {
+            SetErrorMessage([[error localizedDescription] UTF8String]);
+        }
+    }
+
+    void HandleOKCallback()
+    {
+        NanScope();
+
+        Handle<Value> args[2] = { NanNull(), NanNew<v8::Array>([this->tags count]) };
+
+        int i = 0;
+        for (NSString *item in this->tags) {
+            args[1].As<Array>()->Set(i++, NanNew([item UTF8String]));
+        }
+
+        callback->Call(2, args);
+    }
+private:
+    const char *path;
+    NSMutableSet *tags;
+};
+
+class AddTagsWorker : public NanAsyncWorker {
+public:
+    AddTagsWorker(NanCallback *callback, const char *path, NSMutableSet *tags) : NanAsyncWorker(callback), path(path), tags(tags) {}
+    ~AddTagsWorker() {}
+
+    void Execute()
+    {
+        NSString *s = [NSString stringWithUTF8String:this->path];
+        NSURL *url = [NSURL fileURLWithPath:s];
+        NSError *error = nil;
+        NSMutableSet *oldTags = nil;
+        NSArray *oldTagsArray;
+
+        [url getResourceValue:&oldTagsArray forKey:NSURLTagNamesKey error:&error];
+
+        if (error) {
+            return SetErrorMessage([[error localizedDescription] UTF8String]);
+        } else {
+            oldTags = [NSMutableSet setWithArray:oldTagsArray];    
+        }
+
+        [tags unionSet:oldTags];
+
+        [url setResourceValue:[tags allObjects] forKey:NSURLTagNamesKey error:&error];
+
+        if (error) {
+            SetErrorMessage([[error localizedDescription] UTF8String]);
+        }
+    }
+private:
+    const char *path;
+    NSMutableSet *tags;
+};
+
+class RemoveTagsWorker : public NanAsyncWorker {
+public:
+    RemoveTagsWorker(NanCallback *callback, const char *path, NSMutableSet *tags) : NanAsyncWorker(callback), path(path), tags(tags) {}
+    ~RemoveTagsWorker() {}
+
+    void Execute()
+    {
+        NSString *s = [NSString stringWithUTF8String:this->path];
+        NSURL *url = [NSURL fileURLWithPath:s];
+        NSError *error = nil;
+        NSMutableSet *oldTags = nil;
+        NSArray *oldTagsArray;
+
+        [url getResourceValue:&oldTagsArray forKey:NSURLTagNamesKey error:&error];
+
+        if (error) {
+            return SetErrorMessage([[error localizedDescription] UTF8String]);
+        } else {
+            oldTags = [NSMutableSet setWithArray:oldTagsArray];    
+        }
+
+        [oldTags minusSet:tags];
+
+        [url setResourceValue:[oldTags allObjects] forKey:NSURLTagNamesKey error:&error];
+
+        if (error) {
+            SetErrorMessage([[error localizedDescription] UTF8String]);
+        }
+    }
+private:
+    const char *path;
+    NSMutableSet *tags;
+};
+
+NAN_METHOD(getTags)
 {
-    async_data* data = static_cast<async_data*>(req->data);    
-
-    NSString* s = [NSString stringWithUTF8String:data->path];
-    NSURL* url = [NSURL fileURLWithPath:s];
-    NSError* error = nil;
-    NSArray* tags;
-    
-    [url getResourceValue:&tags forKey:NSURLTagNamesKey error:&error];
-
-    if (error) {
-        NSString* errorText = [error localizedDescription];
-        int length = [errorText lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
-        data->error = strcpy(new char[length], [errorText UTF8String]);
-    }
-
-    data->tags = [NSMutableSet setWithArray:tags];    
-}
-
-void workSetTags(uv_work_t* req)
-{    
-    async_data* data = static_cast<async_data*>(req->data);
-
-    NSString* s = [NSString stringWithUTF8String:data->path];
-    NSURL* url = [NSURL fileURLWithPath:s];
-    NSError* error = nil;
-    
-    [url setResourceValue:[data->tags allObjects] forKey:NSURLTagNamesKey error:&error];
-
-    if (error) {
-        NSString* errorText = [error localizedDescription];
-        int length = [errorText lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
-        data->error = strcpy(new char[length], [errorText UTF8String]);
-    }
-}
-
-void workAddTags(uv_work_t* req)
-{
-    workGetTags(req);
-
-    async_data* data = static_cast<async_data*>(req->data);
-    [data->updatingTags unionSet:data->tags];
-    data->tags = data->updatingTags;
-
-    workSetTags(req);
-}
-
-void workRemoveTags(uv_work_t* req)
-{
-    workGetTags(req);
-
-    async_data* data = static_cast<async_data*>(req->data);
-    [data->tags minusSet:data->updatingTags];
-
-    workSetTags(req);
-}
-
-void afterGetTags(uv_work_t* req, int status)
-{
-    HandleScope scope;
-
-    async_data* data = static_cast<async_data*>(req->data);
-
-    TryCatch try_catch;
-    Handle<Value> error;
-    if (data->error) {        
-        error = Exception::Error(String::New(data->error));
-    } else {
-        error = Null();
-    }
-    Handle<Value> args[2] = { error, Array::New([data->tags count]) };
-
-    int i = 0;
-    for (NSString* item in data->tags) {
-        args[1].As<Array>()->Set(i++, String::New([item UTF8String]));
-    }
-
-    data->callback->Call(Context::GetCurrent()->Global(), 2, args);
-    if (try_catch.HasCaught()) {
-        node::FatalException(try_catch);
-    }
-
-    data->callback.Dispose();
-
-    delete data->path;
-    delete data;
-    delete req;
-}
-
-void afterSetTags(uv_work_t* req, int status)
-{
-    HandleScope scope;
-    async_data* data = static_cast<async_data*>(req->data);
-
-    TryCatch try_catch;
-    Handle<Value> error;
-    if (data->error) {
-        error = Exception::Error(String::New(data->error));
-    } else {
-        error = Null();
-    }
-    Handle<Value> args[1] = { error };
-
-    data->callback->Call(Context::GetCurrent()->Global(), 1, args);
-    if (try_catch.HasCaught()) {
-        node::FatalException(try_catch);
-    }
-
-    data->callback.Dispose();
-
-    delete data->path;
-    delete data;
-    delete req;
-}
-
-void afterAddTags(uv_work_t* req, int status)
-{
-    HandleScope scope;
-    async_data* data = static_cast<async_data*>(req->data);
-
-    TryCatch try_catch;
-    Handle<Value> error;
-    if (data->error) {
-        error = Exception::Error(String::New(data->error));
-    } else {
-        error = Null();
-    }
-    Handle<Value> args[1] = { error };
-
-    data->callback->Call(Context::GetCurrent()->Global(), 1, args);
-    if (try_catch.HasCaught()) {
-        node::FatalException(try_catch);
-    }
-
-    data->callback.Dispose();
-
-    delete data->path;
-    delete data;
-    delete req;
-}
-
-void afterRemoveTags(uv_work_t* req, int status)
-{
-    HandleScope scope;
-    async_data* data = static_cast<async_data*>(req->data);
-
-    TryCatch try_catch;
-    Handle<Value> error;
-    if (data->error) {
-        error = Exception::Error(String::New(data->error));
-    } else {
-        error = Null();
-    }
-    Handle<Value> args[1] = { error };
-
-    data->callback->Call(Context::GetCurrent()->Global(), 1, args);
-    if (try_catch.HasCaught()) {
-        node::FatalException(try_catch);
-    }
-
-    data->callback.Dispose();
-
-    delete data->path;
-    delete data;
-    delete req;
-}
-
-Handle<Value> getTags(const Arguments& args)
-{
-    HandleScope scope;
+    NanScope();
 
     if (args.Length() < 2) {
-        ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-        return scope.Close(Undefined());
+        NanThrowTypeError("Wrong number of arguments");
+        NanReturnUndefined();
     }
     if (!args[0]->IsString()) {
-        ThrowException(Exception::TypeError(String::New("Path must be a string")));
-        return scope.Close(Undefined());
+        NanThrowTypeError("Path must be a string");
+        NanReturnUndefined();
     }
     if (!args[1]->IsFunction()) {
-        ThrowException(Exception::TypeError(String::New("Callback must be a function")));
-        return scope.Close(Undefined());
+        NanThrowTypeError("Callback must be a function");
+        NanReturnUndefined();
     }
 
-    async_data* data = new async_data;
-    data->path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
-    data->callback = Persistent<Function>::New(args[1].As<Function>());
-    data->error = NULL;
+    const char *path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
+    NanCallback *callback = new NanCallback(args[1].As<Function>());
 
-    uv_work_t *req = new uv_work_t;
-    req->data = data;
+    NanAsyncQueueWorker(new GetTagsWorker(callback, path));
 
-    uv_queue_work(uv_default_loop(), req, workGetTags, (uv_after_work_cb)afterGetTags);
-
-    return scope.Close(Undefined());
+    NanReturnUndefined();
 }
 
-Handle<Value> setTags(const Arguments& args)
+NAN_METHOD(setTags)
 {
-    HandleScope scope;
-
-    if (args.Length() < 3) {
-        ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-        return scope.Close(Undefined());
-    }
-    if (!args[0]->IsString()) {
-        ThrowException(Exception::TypeError(String::New("Path must be a string")));
-        return scope.Close(Undefined());
-    }
-    if (!args[1]->IsArray()) {
-        ThrowException(Exception::TypeError(String::New("Tags must be an array")));
-        return scope.Close(Undefined());
-    }
-    if (!args[2]->IsFunction()) {
-        ThrowException(Exception::TypeError(String::New("Callback must be a function")));
-        return scope.Close(Undefined());
-    }
-
-    async_data* data = new async_data;
-    data->path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
-    data->callback = Persistent<Function>::New(args[2].As<Function>());
-    data->tags = [NSMutableSet new];
-    data->error = NULL;
-
-    Local<Array> tags = args[1].As<Array>();
-    for (uint32_t i = 0; i < tags->Length(); i++) {
-        if (!tags->Get(i)->IsString()) {
-            continue;
-        }
-
-        [data->tags addObject:[NSString stringWithUTF8String:*(String::Utf8Value(tags->Get(i)))]];
-    }
-
-    uv_work_t *req = new uv_work_t;
-    req->data = data;
-
-
-    uv_queue_work(uv_default_loop(), req, workSetTags, (uv_after_work_cb)afterSetTags);
-
-    return scope.Close(Undefined());
-}
-
-Handle<Value> addTags(const Arguments& args)
-{
-    HandleScope scope;
-
-    if (args.Length() < 3) {
-        ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-        return scope.Close(Undefined());
-    }
-    if (!args[0]->IsString()) {
-        ThrowException(Exception::TypeError(String::New("Path must be a string")));
-        return scope.Close(Undefined());
-    }
-    if (!args[1]->IsArray()) {
-        ThrowException(Exception::TypeError(String::New("Tags must be an array")));
-        return scope.Close(Undefined());
-    }
-    if (!args[2]->IsFunction()) {
-        ThrowException(Exception::TypeError(String::New("Callback must be a function")));
-        return scope.Close(Undefined());
-    }
-
-    async_data* data = new async_data;
-    data->path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
-    data->callback = Persistent<Function>::New(args[2].As<Function>());
-    data->updatingTags = [NSMutableSet new];
-    data->error = NULL;
-
-    Local<Array> updatingTags = args[1].As<Array>();
-    for (uint32_t i = 0; i < updatingTags->Length(); i++) {
-        if (!updatingTags->Get(i)->IsString()) {
-            continue;
-        }
-
-        [data->updatingTags addObject:[NSString stringWithUTF8String:*(String::Utf8Value(updatingTags->Get(i)))]];
-    }
-
-    uv_work_t *req = new uv_work_t;
-    req->data = data;
-
-    uv_queue_work(uv_default_loop(), req, workAddTags, (uv_after_work_cb)afterAddTags);
-
-    return scope.Close(Undefined());
-}
-
-Handle<Value> removeTags(const Arguments& args)
-{
-    HandleScope scope;
+    NanScope();
 
     if (args.Length() < 2) {
-        ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-        return scope.Close(Undefined());
+        NanThrowTypeError("Wrong number of arguments");
+        NanReturnUndefined();
     }
     if (!args[0]->IsString()) {
-        ThrowException(Exception::TypeError(String::New("Path must be a string")));
-        return scope.Close(Undefined());
+        NanThrowTypeError("Path must be a string");
+        NanReturnUndefined();
     }
     if (!args[1]->IsArray()) {
-        ThrowException(Exception::TypeError(String::New("Tags must be an array")));
-        return scope.Close(Undefined());
+        NanThrowTypeError("Tags must be an array");
+        NanReturnUndefined();
     }
-    if (!args[2]->IsFunction()) {
-        ThrowException(Exception::TypeError(String::New("Callback must be a function")));
-        return scope.Close(Undefined());
+    if (args.Length() >= 3 && !args[2]->IsFunction()) {
+        NanThrowTypeError("Callback must be a function");
+        NanReturnUndefined();
     }
 
-    async_data* data = new async_data;
-    data->path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
-    data->callback = Persistent<Function>::New(args[2].As<Function>());
-    data->updatingTags = [NSMutableSet new];
-    data->error = NULL;
+    const char *path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
+    NanCallback *callback = new NanCallback(args[2].As<Function>());
+    NSMutableSet *tags = [NSMutableSet new];
 
-    Local<Array> updatingTags = args[1].As<Array>();
-    for (uint32_t i = 0; i < updatingTags->Length(); i++) {
-        if (!updatingTags->Get(i)->IsString()) {
+    Local<Array> t = args[1].As<Array>();
+    for (uint32_t i = 0; i < t->Length(); i++) {
+        if (!t->Get(i)->IsString()) {
             continue;
         }
 
-        [data->updatingTags addObject:[NSString stringWithUTF8String:*(String::Utf8Value(updatingTags->Get(i)))]];
+        [tags addObject:[NSString stringWithUTF8String:*(String::Utf8Value(t->Get(i)))]];
     }
 
-    uv_work_t *req = new uv_work_t;
-    req->data = data;
+    NanAsyncQueueWorker(new SetTagsWorker(callback, path, tags));
 
-    uv_queue_work(uv_default_loop(), req, workRemoveTags, (uv_after_work_cb)afterRemoveTags);
+    NanReturnUndefined();
+}
 
-    return scope.Close(Undefined());
+NAN_METHOD(addTags)
+{
+    NanScope();
+
+    if (args.Length() < 2) {
+        NanThrowTypeError("Wrong number of arguments");
+        NanReturnUndefined();
+    }
+    if (!args[0]->IsString()) {
+        NanThrowTypeError("Path must be a string");
+        NanReturnUndefined();
+    }
+    if (!args[1]->IsArray()) {
+        NanThrowTypeError("Tags must be an array");
+        NanReturnUndefined();
+    }
+    if (args.Length() >= 3 && !args[2]->IsFunction()) {
+        NanThrowTypeError("Callback must be a function");
+        NanReturnUndefined();
+    }
+
+    const char *path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
+    NanCallback *callback = new NanCallback(args[2].As<Function>());
+    NSMutableSet *tags = [NSMutableSet new];
+
+    Local<Array> t = args[1].As<Array>();
+    for (uint32_t i = 0; i < t->Length(); i++) {
+        if (!t->Get(i)->IsString()) {
+            continue;
+        }
+
+        [tags addObject:[NSString stringWithUTF8String:*(String::Utf8Value(t->Get(i)))]];
+    }
+
+    NanAsyncQueueWorker(new AddTagsWorker(callback, path, tags));
+
+    NanReturnUndefined();
+}
+
+NAN_METHOD(removeTags)
+{
+    NanScope();
+
+    if (args.Length() < 2) {
+        NanThrowTypeError("Wrong number of arguments");
+        NanReturnUndefined();
+    }
+    if (!args[0]->IsString()) {
+        NanThrowTypeError("Path must be a string");
+        NanReturnUndefined();
+    }
+    if (!args[1]->IsArray()) {
+        NanThrowTypeError("Tags must be an array");
+        NanReturnUndefined();
+    }
+    if (args.Length() >= 3 && !args[2]->IsFunction()) {
+        NanThrowTypeError("Callback must be a function");
+        NanReturnUndefined();
+    }
+
+    const char *path = strcpy(new char[String::Utf8Value(args[0]).length()], *(String::Utf8Value(args[0])));
+    NanCallback *callback = new NanCallback(args[2].As<Function>());
+    NSMutableSet *tags = [NSMutableSet new];
+
+    Local<Array> t = args[1].As<Array>();
+    for (uint32_t i = 0; i < t->Length(); i++) {
+        if (!t->Get(i)->IsString()) {
+            continue;
+        }
+
+        [tags addObject:[NSString stringWithUTF8String:*(String::Utf8Value(t->Get(i)))]];
+    }
+
+    NanAsyncQueueWorker(new RemoveTagsWorker(callback, path, tags));
+
+    NanReturnUndefined();
 }
 
 void init(Handle<Object> exports)
 {
-    exports->Set(String::NewSymbol("getTags"),
-        FunctionTemplate::New(getTags)->GetFunction());
-    exports->Set(String::NewSymbol("setTags"),
-        FunctionTemplate::New(setTags)->GetFunction());
-    exports->Set(String::NewSymbol("addTags"),
-        FunctionTemplate::New(addTags)->GetFunction());
-    exports->Set(String::NewSymbol("removeTags"),
-        FunctionTemplate::New(removeTags)->GetFunction());
+    exports->Set(NanNew("getTags"),
+        NanNew<FunctionTemplate>(getTags)->GetFunction());
+    exports->Set(NanNew("setTags"),
+        NanNew<FunctionTemplate>(setTags)->GetFunction());
+    exports->Set(NanNew("addTags"),
+        NanNew<FunctionTemplate>(addTags)->GetFunction());
+    exports->Set(NanNew("removeTags"),
+        NanNew<FunctionTemplate>(removeTags)->GetFunction());
 }
 
 NODE_MODULE(osx_tag, init)
